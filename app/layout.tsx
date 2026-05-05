694fe2c868f7787448ef70a1c90c9d6c6d0bcbb3
ziadabbasi26 <ziadabbasi26@gmail.com>
2026-05-05 16:21:43 +0500

    fix layout types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "E-COMMERCE STORE",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="body">
